47b71613c537a57c2c8a53b8025ade7b7926fbab
Алена Карпова <alenapancenko00@gmail.com>
2022-11-01 23:58:33 +0300

    init

---
+++ VK/VK/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Алена Панченко on 01.11.2022.
+//
+
+import UIKit
+
+/// VC
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        func add() {}
+    }
+}
